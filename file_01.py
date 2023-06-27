425279c270eea9d014cc9c416269278a7535cf81
Felipe Leite <felipesd.leite@gmail.com>
2023-06-27 11:16:05 -0300

    Adding comments about ways to write a comment or note in python

---
+++ file_01.py
@@ -0,0 +1,12 @@
+# printing a comment on screen
+
+print(123) # This print the value 123, and python will understand the print value as a int type
+print(type('pedro')) # Here we have a print of the values type, its will return a <class 'str'>
+
+"""
+    This notation is used as a way to make notes, and its called as DocString. Its not a comment.
+"""
+
+'''
+    This can use to make notes too, as double quotes notation. Its a DOcString to
+'''
